bfeb6c075d6a55be368eb0a8294377b2a29fe101
Erik Bye <Bye@MinistryCentered.com>
2021-04-07 11:11:59 -0700

    Remove pod files that are now in git ignore

--- Pods/Target Support Files/Pods-Loper-LoperTests/Pods-Loper-LoperTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Loper_LoperTests : NSObject
-@end
-@implementation PodsDummy_Pods_Loper_LoperTests
-@end
--- Pods/Target Support Files/Pods-Loper/Pods-Loper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Loper : NSObject
-@end
-@implementation PodsDummy_Pods_Loper
-@end
